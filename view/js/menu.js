2cfafc27b9c8bd231b1349b10f34c3505db1c1ca
andersonmarioneto <netoanderson192@gmail.com>
2024-06-16 17:35:57 +0100

    12 de junho

---
+++ view/js/menu.js
@@ -0,0 +1,33 @@
+/* by: Anderson Mário Neto | UI Designer, Front-End */
+
+
+/*==================== seções de rolagem link ativo =====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 50,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== Alterar cabeçalho em segundo plano ====================*/ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) 
+        header.classList.add('scroll-header'); 
+    else 
+    header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
